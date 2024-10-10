e7677716071bd5a4900cec18ce3d20023ab702da
Papali Priya Ratan Sahu <sahu.ppr@gmail.com>
2024-10-10 17:40:50 +0530

    FIXED THE ERROR

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -6,18 +5,7 @@ import MakeYourSpace from "./MakeYourSpace";
 import Home from './Home';
 import ScrollIndicator from './ScrollIndicator';
 import PreloaderContainer from './PreloaderContainer'; // For Preloader
-
 import './style.css';
-import './script.js'; 
-=======
-import Home from './Home';
-import ScrollIndicator from './ScrollIndicator';
-import PreloaderContainer from './PreloaderContainer.js'; // For Preloader
-import './style.css';
-import './script.js';
->>>>>>> a6950484b0bf1fde89acd194824dd4c626168aba
-import './preloader.js';
-import './TopBar.js';
 
 function App() {
   return (
