48fc089587347c85995c90899161a925146312d6
rajgharat07 <gharatyashraj@gmail.com>
2024-10-12 10:00:30 +0530

    Bug Fixed : Added 'Scroll to Top' button on Services and Blog pages for improved user experience.

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -136,6 +136,34 @@ const Blogs = () => {
                             </div>
                         ))}
                     </div>
+                    <a
+                    href="#top"
+                    className="back-top-btn active"
+                    aria-label="back to top"
+                    data-back-top-btn
+                    style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textDecoration: "none",
+                    }}
+                >
+                    <ion-icon name="chevron-up" aria-hidden="true"></ion-icon>
+                    <img
+                    src="./images/Vector 2.png"
+                    alt="Back to top"
+                    className="caret-image"
+                    />
+                </a>
+                <script src="./script.js"></script>
+                <script
+                    type="module"
+                    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+                ></script>
+                <script
+                    nomodule
+                    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+                ></script>
                 </div>
             </section>
         </>
