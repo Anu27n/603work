68f475f5b40316b1070d724b6c21e6ec37c705c3
k7 <kesavanvel77@gmail.com>
2024-10-22 22:32:49 +0530

    seperate route files for efficient usage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home.js";
-import BlogPost from "./Pages/Blogs/BlogPost";
-import Blogs from "./Pages/Blogs/Blogs";
-import Contact from "./Pages/Contact/Contact";
-import Image from "./Pages/Photos/Photos.js";
-import Services from "./Pages/Services/Services";
-import "./Components/preloader.js";
-import PreloaderContainer from "./Components/PreloaderContainer.js"; // For Preloader
-import "./Script/script.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import PreloaderContainer from "./Components/PreloaderContainer.js";
 import ScrollIndicator from "./Components/ScrollIndicator.js";
+import AppRoutes from "./Routes/AppRoutes.js";
 import "./styles/style.css";
+import "./Components/preloader.js";
+import "./Script/script.js";
 import "./Components/TopBar.js";
 
 function App() {
@@ -18,18 +13,7 @@ function App() {
       <ScrollIndicator />
       <PreloaderContainer>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blogs/:id" element={<BlogPost />} />
-            <Route path="/photos" element={<Image />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route
-              path="/book-a-space"
-              element={"This Page is under construction"}
-            />
-          </Routes>
+          <AppRoutes />
         </Router>
       </PreloaderContainer>
     </div>
