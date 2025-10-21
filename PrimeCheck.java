1bd25798441da9bab7f11dc26f61fefa329310a0
Kushagra Kapoor <kk3720@srmist.edu.in>
2025-10-21 12:27:39 +0530

    Create PrimeCheck.java

---
+++ PrimeCheck.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        sc.close();
+
+        boolean isPrime = true;
+
+        // 0 and 1 are not prime numbers
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // check divisibility from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }
+}
